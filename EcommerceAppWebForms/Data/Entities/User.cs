e43584a5d81bfae92a8caade49fd0848e50b1afd
TelmanYusifov <43857129+TelmanYusifov@users.noreply.github.com>
2020-02-28 23:32:55 +0400

    User implemented.

--- EcommerceAppWebForms/Data/Entities/User.cs
+++ EcommerceAppWebForms/Data/Entities/User.cs
@@ -2,5 +2,16 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+    public enum Role
+    {
+        User,
+        Admin
     }
 }
