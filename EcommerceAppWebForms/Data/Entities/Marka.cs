037def5cec05fc324e5de39ed0ab2a3e36f46497
TelmanYusifov <43857129+TelmanYusifov@users.noreply.github.com>
2020-02-28 22:49:48 +0400

    Model and marka added.

---
+++ EcommerceAppWebForms/Data/Entities/Marka.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceAppWebForms.Data.Entities
+{
+    public class Marka
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 30, MinimumLength = 3)]
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+        public Marka()
+        {
+            Models = new HashSet<Model>();
+        }
+    }
+}
---
+++ EcommerceAppWebForms/Data/Entities/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceAppWebForms.Data.Entities
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 30, MinimumLength = 3)]
+        public string Name { get; set; }
+        public Marka marka { get; set; }
+        public int MarkaId { get; set; }
+    }
+}
